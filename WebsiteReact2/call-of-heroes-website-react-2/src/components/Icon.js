fa2cdc50ae4a6a995fba8bcdd90be9cfebce0dd7
David Irimia <36113284+daverave1212@users.noreply.github.com>
2025-04-03 18:23:54 +0300

    Added login system with Firebase (2/2)

--- WebsiteReact2/call-of-heroes-website-react-2/src/components/Icon.js
+++ WebsiteReact2/call-of-heroes-website-react-2/src/components/Icon.js
@@ -1,13 +1,17 @@
 import React from 'react'
 
-export default function Icon({ name, type }) {
+export default function Icon({ name, type, style }) {
 
     const className = 
         type == 'spell' ? 'inline-icon--spell' :
         type == 'small-stat' ? 'inline-icon--small-stat' :
         'inline-icon'
 
+    if (name.includes('.') == false) {
+        name == '.png'
+    }
+
     return (
-        <img className={className} src={`/Icons/UI/${name}.png`}/>
+        <img className={className} src={`/Icons/UI/${name}`} style={style}/>
     )
 }
